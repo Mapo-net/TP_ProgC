4fef6c0a4554bb324bd8f25feb2b17402bbb8cfd
MaPo <240580614+Mapo-net@users.noreply.github.com>
2026-09-18 09:50:44 +0000

    changement du code calcul.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -3,5 +3,47 @@
 #include <string.h>
 
 int main() {
+	int num1 = 16;
+	int num2 = 3;
+	char op = '/';
 
+	switch (op) {
+		case '+':
+			printf("%d + %d = %d\n", num1, num2, num1 + num2);
+			break;
+		case '-':
+			printf("%d - %d = %d\n", num1, num2, num1 - num2);
+			break;
+		case '*':
+			printf("%d * %d = %d\n", num1, num2, num1 * num2);
+			break;
+		case '/':
+			if (num2 == 0) {
+				printf("Erreur : division par zero impossible.\n");
+			} else {
+				printf("%d / %d = %d\n", num1, num2, num1 / num2);
+			}
+			break;
+		case '%':
+			if (num2 == 0) {
+				printf("Erreur : modulo par zero impossible.\n");
+			} else {
+				printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+			}
+			break;
+		case '&':
+			printf("%d & %d = %d\n", num1, num2, num1 & num2);
+			break;
+		case '|':
+			printf("%d | %d = %d\n", num1, num2, num1 | num2);
+			break;
+		case '~':
+			printf("~%d = %d\n", num1, ~num1);
+			break;
+		default:
+			printf("Operateur inconnu : %c\n", op);
+			return 1;
+	}
+
+	return 0;
 }
