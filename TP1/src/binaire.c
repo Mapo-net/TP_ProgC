0c09b30d47ea32be63800628b0d843e15622c717
MaPo <240580614+Mapo-net@users.noreply.github.com>
2026-09-18 09:57:31 +0000

    changement du code binaire.c

--- TP1/src/binaire.c
+++ TP1/src/binaire.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
+	int nombres[] = {0, 4096, 65536, 65535, 1024};
+	int nombreDeNombres = sizeof(nombres) / sizeof(nombres[0]);
 
+	for (int index = 0; index < nombreDeNombres; index++) {
+		int valeur = nombres[index];
+		int bits[32];
+		int nombreDeBits = 0;
+
+		if (valeur == 0) {
+			bits[nombreDeBits++] = 0;
+		} else {
+			for (; valeur > 0; valeur /= 2) {
+				bits[nombreDeBits++] = valeur % 2;
+			}
+		}
+
+		printf("%d en binaire : ", nombres[index]);
+		for (int bit = nombreDeBits - 1; bit >= 0; bit--) {
+			printf("%d", bits[bit]);
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
