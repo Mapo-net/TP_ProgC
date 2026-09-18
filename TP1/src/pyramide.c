c715ae2c64c54eaa2c1436a1f820e4f93d0c3163
MaPo <240580614+Mapo-net@users.noreply.github.com>
2026-09-18 10:01:20 +0000

    changement du code pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,7 +1,26 @@
 
 #include <stdio.h>
-#include <string.h>
 
 int main() {
+	int n = 5;
 
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			printf(" ");
+		}
+
+		for (int j = 1; j <= i; j++) {
+			printf("%d", j);
+		}
+
+		for (int j = i - 1; j >= 1; j--) {
+			printf("%d", j);
+		}
+
+		printf("\n");
+	}
+
+	printf("Generation de la pyramide terminee.\n");
+
+	return 0;
 }
