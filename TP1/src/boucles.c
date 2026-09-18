7f0d3557d90c98041d506b056c3e1416d99d96df
MaPo <240580614+Mapo-net@users.noreply.github.com>
2026-09-18 09:34:28 +0000

    changement du code boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -2,5 +2,23 @@
 #include <string.h>
 
 int main() {
+	int compteur = 5;
 
+	if (compteur >= 10) {
+		printf("La valeur de compteur doit etre strictement inferieure a 10.\n");
+		return 1;
+	}
+
+	for (int ligne = 1; ligne <= compteur; ligne++) {
+		for (int colonne = 1; colonne <= ligne; colonne++) {
+			if (ligne == 1 || ligne == compteur || colonne == 1 || colonne == ligne) {
+				printf("* ");
+			} else {
+				printf("# ");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
