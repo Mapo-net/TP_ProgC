a62c03204af081074f54d4506b6dfdbf11406c91
MaPo <240580614+Mapo-net@users.noreply.github.com>
2026-09-18 09:30:34 +0000

    changement du code opérateur.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,25 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    int a = 16;
+    int b = 3;
+    
+    int addition = a + b;
+    int soustraction = a - b;
+    int multiplication = a * b;
+    float division = (float) a / b;
+    int modulo = a % b;
+    int comparaisonEgal = a == b;
+    int comparaisonSup = a > b;
 
+    printf("addition : %d\n", addition);
+    printf("soustraction : %d\n", soustraction);
+    printf("multiplication : %d\n", multiplication);
+    printf("division : %f\n", division);
+    printf("modulo : %d\n", modulo);
+    printf("a est egal a b : %d\n", comparaisonEgal);
+    printf("a est superieur a b : %d\n", comparaisonSup);
+
+    return 0;
+}
